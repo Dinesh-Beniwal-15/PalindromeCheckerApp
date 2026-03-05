656e9ab4c49d38d60ccc315ba02ee0ac0eabbc3b
Dinesh-Beniwal-15 <dineshbeniwal1515@gmail.com>
2026-03-05 09:40:43 +0530

    main branch

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,37 +2,4 @@ import java.util.*;
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("=== UC7: Deque Based Palindrome Check ===");
-
-        System.out.print("Enter a word: ");
-        String input = scanner.nextLine();
-
-        Deque<Character> deque = new LinkedList<>();
-
-        // Add characters to deque
-        for(char c : input.toCharArray()) {
-            deque.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-        // Compare from both ends
-        while(deque.size() > 1) {
-            if(deque.removeFirst() != deque.removeLast()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if(isPalindrome) {
-            System.out.println(input + " is a Palindrome (Deque Method)");
-        } else {
-            System.out.println(input + " is NOT a Palindrome");
-        }
-
-        scanner.close();
-    }
 }
